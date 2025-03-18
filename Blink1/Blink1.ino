0b4a1321e07a5e097954c7565cd22e97a9ca2a3c
FattoBulbins <156241539+brs288@users.noreply.github.com>
2025-03-18 15:36:04 -0400

    1895 Assignment #15 - Introduction to Git
    This commit includes the source code for the 1895 Assignment #9 - Hello World. This source code will be used in different branches created by each member of the team and ultimately merged to complete the tasks in assignment #15.

---
+++ Blink1/Blink1.ino
@@ -0,0 +1,15 @@
+// One time setup
+void setup() {
+  pinMode(8, OUTPUT);
+  pinMode(A5, INPUT);
+}
+
+// Loop through
+void loop() {
+    if (digitalRead(A5) == LOW) {
+        digitalWrite(8, HIGH);
+        delay(1000);
+        digitalWrite(8, LOW);
+        delay(1000);
+    }
+}
---
+++ Blink3/Blink3.ino
@@ -0,0 +1,25 @@
+// One time setup
+void setup() {
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(A5, INPUT);
+}
+
+// Loop through
+void loop() {
+    if (digitalRead(A5) == LOW) {
+        digitalWrite(6, HIGH);
+        delay(1000);
+        digitalWrite(6, LOW);
+        delay(1000);
+        digitalWrite(7, HIGH);
+        delay(1000);
+        digitalWrite(7, LOW);
+        delay(1000);
+        digitalWrite(8, HIGH);
+        delay(1000);
+        digitalWrite(8, LOW);
+        delay(1000);
+    }
+}
